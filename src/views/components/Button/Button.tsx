d521e2c686cd03aacb8ea807f965226acf608a9f
re7za <reza.q@bimebazar.com>
2022-12-21 17:35:47 +0330

    Add Button comp

---
+++ src/views/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { createElement, ElementType, FunctionComponent, HTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface IButtonProps extends HTMLAttributes<Element> {
+  component?: ElementType
+  iconBtn?: boolean
+  disabled?: boolean
+}
+
+const Button: FunctionComponent<IButtonProps> = (props) => {
+  const { component = 'button', className, iconBtn, ...otherProps } = props
+  const { disabled } = otherProps
+
+  const defaultStyle = twMerge(
+    `
+    bg-secondary px-6 py-2 dark:bg-white 
+    cursor-pointer rounded transition
+    text-white dark:text-primary  
+    font-medium
+  `,
+    disabled ? 'bg-grey-600 ' : '',
+  )
+  const iconBtnStyle = iconBtn ? `rounded-full px-2 py-2` : ''
+
+  return createElement(component, {
+    className: twMerge(defaultStyle, iconBtnStyle, className),
+    ...otherProps,
+  })
+}
+
+export default Button
