f16318d870ccd84d6b5a8d4076afc73bd7230d54
re7za <reza.q@bimebazar.com>
2022-12-24 00:48:58 +0330

    Format time with extra 0 in it's string

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,9 @@
+function padding(num: number) {
+  return ('0' + num).slice(-2)
+}
+
 export const getDateWithTimeString = (date: Date, withTime = false): string => {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}${
-    withTime ? ` - ${date.getHours()}:${date.getMinutes()}` : ''
+    withTime ? ` - ${padding(date.getHours())}:${padding(date.getMinutes())}` : ''
   }`
 }
